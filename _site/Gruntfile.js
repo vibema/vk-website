42ea0f5f176f64344968c6cbdac46bbbaa6203f2
Viktoria Knuth <100833204+vibema@users.noreply.github.com>
2024-05-28 17:01:01 +0200

    Update status

--- _site/Gruntfile.js
+++ _site/Gruntfile.js
@@ -1,91 +1,83 @@
 const sass = require('sass');
 
 module.exports = function(grunt) {
-    // Project configuration
-    grunt.initConfig({
-      sass: {
-        options: {
-          implementation: sass,
-          sourceMap: true
-        },
-        base: {
-          files: {
-            'assets/css/base.css': 'sass/base.scss'
-          }
-        },
-        home: {
-          files: {
-            'assets/css/home.css': 'sass/home.scss'
+  // Project configuration
+  grunt.initConfig({
+    sass: {
+      options: {
+        implementation: sass,
+        sourceMap: true
+      },
+      base: {
+        files: {
+          'assets/css/base.css': 'sass/base.scss'
+        }
+      },
+      home: {
+        files: {
+          'assets/css/home.css': 'sass/home.scss'
         }
       }
-      },
-      postcss: {
+    },
+    jekyll: {
+      build: {},
+      serve: {
         options: {
-          map: true,
-          processors: [
-            require('tailwindcss'),
-            require('autoprefixer')
+          serve: true,
+          watch: true
+        }
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            'assets/css/*.css',
+            '**/*.html',
+            '**/*.js'
           ]
         },
-        dist: {
-          src: 'styles.css',
-          dest: 'assets/css/styles.css'
+        options: {
+          watchTask: true,
+          server: '_site'
+        }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 2222,
+          base: '_site', // The directory where Jekyll generates the static files
+          livereload: true,
+          open: true
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['sass/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
         }
       },
       jekyll: {
-        build: {},
-        serve: {
-          options: {
-            serve: true,
-            watch: true
-          }
-        }
-      },
-      browserSync: {
-        dev: {
-          bsFiles: {
-            src : [
-              'assets/css/*.css',
-              '**/*.html',
-              '**/*.js'
-            ]
-          },
-          options: {
-            watchTask: true,
-            server: '_site'
-          }
-        }
-      },
-      connect: {
-        server: {
-          options: {
-            port: 2222,
-            base: '_site', // The directory where Jekyll generates the static files
-            livereload: true,
-            open: true
-          }
-        }
-      },
-      watch: {
-        jekyll: {
-          files: ['**/*.{html,md,markdown,css,scss,js}'],
-          tasks: ['sass','jekyll:build'],
-          options: {
-            spawn: false,
-          }
+        files: ['**/*.{html,md,markdown,css,scss,js}'],
+        tasks: ['sass', 'jekyll:build'],
+        options: {
+          spawn: false
         }
       }
-    });
-  
-    // Load Grunt plugins
-    grunt.loadNpmTasks('grunt-browser-sync');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-postcss');
-    grunt.loadNpmTasks('grunt-jekyll');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-  
-    // Default task(s)
-    grunt.registerTask('default', ['sass','postcss','jekyll:build', 'connect','browserSync', 'watch']);
-  };
-  
+    }
+  });
+
+  // Load Grunt plugins
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s)
+  grunt.registerTask('default', ['sass', 'jekyll:build', 'connect', 'browserSync', 'watch']);
+};
